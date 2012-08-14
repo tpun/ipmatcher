57188d587480bb4e49df6d14ba4018f6edd680e1
Thomas Pun <thomas@nowbox.com>
2012-08-13 18:22:09 -0700

    match static explicit ip. (wildcard case currently fails)

---
+++ ipmatcher.rb
@@ -0,0 +1,34 @@
+class IPMatcher
+  attr_reader :patterns
+  def initialize
+    @patterns = {}
+  end
+
+  def add pattern
+    sub_patterns = pattern.split('.')
+    start = @patterns
+    while sub_patterns.count > 0
+      sub_pattern = sub_patterns.shift
+      if sub_patterns.count==0 || sub_pattern=='*'
+        start[sub_pattern] = true
+      else
+        start[sub_pattern] ||= Hash.new
+      end
+      start = start[sub_pattern]
+    end
+  end
+
+  def match? pattern
+    sub_patterns = pattern.split('.')
+    return false if sub_patterns.count != 4
+
+    start = @patterns
+    while sub_patterns.count > 0
+      sub_pattern = sub_patterns.shift
+      return false if start[sub_pattern].nil?
+      start = start[sub_pattern]
+    end
+
+    true
+  end
+end
---
+++ ipmatcher_spec.rb
@@ -0,0 +1,50 @@
+require_relative './ipmatcher.rb'
+
+describe 'Matcher' do
+  subject { IPMatcher.new }
+
+  describe '#add' do
+    context 'when input does not contain wild card' do
+      it 'adds given pattern' do
+        subject.add '1.2.3.4'
+
+        patterns = {'1' => {'2' => {'3' => {'4' => true}}}}
+        subject.patterns.should == patterns
+      end
+    end
+
+    context 'when input contains wild card' do
+      it 'adds given pattern' do
+        subject.add '2.4.*'
+
+        patterns ={'2' => {'4' => {'*' => true}}}
+        subject.patterns.should == patterns
+      end
+    end
+  end
+
+  describe '#match?' do
+    before do
+      subject.add '1.2.3.5'
+      subject.add '1.2.4.5'
+      subject.add '2.*'
+      subject.add '2.3.*'
+    end
+
+    it 'matches 1.2.3.5' do
+      subject.should be_match('1.2.3.5')
+    end
+
+    it 'does not match 1.2.3.4' do
+      subject.should_not be_match('1.2.3.4')
+    end
+
+    it 'matches 2.1.2.3 with 2.*' do
+      subject.should be_match('2.1.2.3')
+    end
+
+    it 'does not match anything less than 4 subnets' do
+      subject.should_not be_match('1.2.3')
+    end
+  end
+end
